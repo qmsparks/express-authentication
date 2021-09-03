3ab064dcaf80518a7ce1f43861a2f8041f906c03
qsparks <q.m.sparks@gmail.com>
2021-09-03 17:11:48 -0500

    creates env variable and adds session and flash middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const layouts = require('express-ejs-layouts');
+const session = require('express-session');
+const flash = require('connect-flash');
+
 const app = express();
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
@@ -11,6 +14,19 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+app.use(session({
+  secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
+  resave: false,             // Save the session even if it's modified, make this false
+  saveUninitialized: true    // If we have a new session, we save it, therefore making that true
+}));
+app.use(flash());            // flash middleware
+app.use((req, res, next) => {
+  console.log(res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
 
 app.get('/', (req, res) => {
   return res.render('index');
